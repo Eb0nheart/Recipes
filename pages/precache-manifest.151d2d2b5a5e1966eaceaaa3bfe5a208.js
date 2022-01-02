edb2248739606f8f3b25dc8182dc085c88546f84
Simon Senger <76154207+Eb0nheart@users.noreply.github.com>
2022-01-03 00:37:44 +0100

    Remove weather remnants
    Also added pages folder for hosting

---
+++ pages/precache-manifest.151d2d2b5a5e1966eaceaaa3bfe5a208.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c32ca1e3e595349e4cbd002f0ee3583e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c5abc9ad79810acd5a2",
+    "url": "/static/css/2.e826bb25.chunk.css"
+  },
+  {
+    "revision": "927d35514f15a56482e8",
+    "url": "/static/css/main.6636e503.chunk.css"
+  },
+  {
+    "revision": "6c5abc9ad79810acd5a2",
+    "url": "/static/js/2.c1cacce4.chunk.js"
+  },
+  {
+    "revision": "f0c1093415b06c9dc10783dbb242bbcc",
+    "url": "/static/js/2.c1cacce4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "927d35514f15a56482e8",
+    "url": "/static/js/main.e06df4c2.chunk.js"
+  },
+  {
+    "revision": "b183dea5c6dc9deffeed",
+    "url": "/static/js/runtime-main.ccdc9b21.js"
+  }
+]);
