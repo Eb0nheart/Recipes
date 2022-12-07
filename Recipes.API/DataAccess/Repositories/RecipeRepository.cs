65caa7ebdadcecc10d58c24fc1c7fdd1d638a468
Simon Senger <76154207+Eb0nheart@users.noreply.github.com>
2022-12-07 15:19:37 +0100

    fuck ef core

---
+++ Recipes.API/DataAccess/Repositories/RecipeRepository.cs
@@ -0,0 +1,54 @@
+﻿namespace Recipes.API.DataAccess.Repositories;
+
+public record Recipe(Guid Id, string Title, string[] Steps, int CookTimeMinutes, int PreparationTimeMinutes);
+
+public interface IRecipeRepository
+{
+    Task<Recipe> GetRecipeAsync(Guid Id);
+
+    Task<List<Recipe>> GetRecipesAsync();
+
+    /// <summary>
+    /// Update if non-existant, otherwise saves as new recipe.
+    /// </summary>
+    /// <param name="recipe"></param>
+    /// <returns></returns>
+    Task SaveRecipeAsync(Recipe recipe);
+
+    Task DeleteRecipeAsync(Recipe recipe);
+}
+
+
+internal class RecipeRepository : IRecipeRepository
+{
+    private readonly ILogger<RecipeRepository> logger;
+    private static List<Recipe> Recipes = new List<Recipe>();
+
+    public RecipeRepository(
+        ILogger<RecipeRepository> logger)
+    {
+        this.logger = logger;
+    }
+
+    public Task<Recipe> GetRecipeAsync(Guid Id)
+    {
+        return Task.FromResult(Recipes.SingleOrDefault(recipe => recipe.Id == Id));
+    }
+
+    public Task<List<Recipe>> GetRecipesAsync()
+    {
+        return Task.FromResult(Recipes);
+    }
+
+    public Task DeleteRecipeAsync(Recipe recipe)
+    {
+        Recipes.Remove(recipe);
+        return Task.CompletedTask;
+    }
+
+    public Task SaveRecipeAsync(Recipe recipe)
+    {
+        Recipes.Add(recipe);
+        return Task.CompletedTask;
+    }
+}
--- Recipes.API/Program.cs
+++ Recipes.API/Program.cs
@@ -1,18 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Recipes.API.DataAccess.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddScoped<IRecipeRepository, RecipeRepository>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+var group = app.MapGroup("recipe")
+    .WithOpenApi();
+
+group.MapGet("{id}", (Guid id, IRecipeRepository repository) =>
+{
+    return repository.GetRecipeAsync(id);
+});
+
+group.MapGet("", (IRecipeRepository repository) => repository.GetRecipesAsync());
+
+group.MapPut("", ([FromBody] Recipe recipe, IRecipeRepository repository) => repository.SaveRecipeAsync(recipe));
 
 app.UseHttpsRedirection();
 
